c16e831a7d2da280a9cc55e1b4b9556236ced8f7
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-16 21:09:25 +0100

    Found something to api on

--- app.py
+++ app.py
@@ -157,6 +157,10 @@ class Readinglist(db.Model):
 
 db.create_all()
 
+class Help(db.model):
+    pass
+    # FUTURE ME: REMEBER TO MAKE THIS SO THAT WHEN SOMEONE SEARCHES FOR HELP ON SOMETHING THEY GET CORRECT AWNSER BACK, IE: ?topic:chemistry&q="electron" GOES TO {"electron": "A subatomic particle with a negative charge and orbits the nuclues in shells"}
+
 def fib(num): 
     result = []
     count = 0
