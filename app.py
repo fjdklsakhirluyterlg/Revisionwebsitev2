acbf8b6d781210f38a0c2e9c4a8531d935ad511d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-16 17:17:12 +0100

    Added something

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 def get_ecenomic_stuff():
     stuff = []
     # GBP TO EUR
