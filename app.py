36aed159a22de1826837ccb07f3e67ea6e08821e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-17 17:06:13 +0100

    I should put these in a test file

--- app.py
+++ app.py
@@ -54,6 +54,9 @@ class Randomz(Resource):
             list = ['You will make at least 5 pounds', 'You will watch mr beast in the next week', 'You will quit your job', 'Your next phone will be able to run Genshin impact']
             return random.choice(list)
         
+        def fact():
+            list = ['Take an angle A, find out what sin(A)^2 + cos(A)^2 is', 'There are more than 6 million parts in an A380', 'Francium is the most reactive element in the world', 'Jeff Bezos could but 2 million tesla model y', 'Japan has the highest debt to GDP ratio in the world with 234%', 'Monaco should score 1.04 on the Human Development Index (HDI), a scale from 0 - 1']
+        
         x = random.randint(0 ,1000)
         y = randoms()
         z = fortune()
