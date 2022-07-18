a7040521e941cf919eca7a9a476120f63ba96c28
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-18 18:35:06 +0100

    I'm bored

--- app.py
+++ app.py
@@ -318,6 +318,18 @@ def coefficient(x,y):
 
     return a,b,c
 
+def generateπfrom_random(n):
+    ins = 0
+    total = 0
+    for i in range(n):
+        x = random.randint(0, 1)
+        y = random.randint(0, 1)
+        distance = x**2 + y**2
+        if distance <= 1:
+            ins += 1
+        total +=1
+    
+    return 4*ins/total
 
 
 @app.route('/')
